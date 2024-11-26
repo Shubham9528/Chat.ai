0be4b62cf2c1b8d861c2a750933e634cbdd3e3bf
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2024-11-26 11:13:12 +0530

    Update(new component added footer)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ChatInterface from "./component/ChatInterface";
+import  Footer from "./component/Footer";
 function App() {
   return (
     <div>
       <ChatInterface />
+      <Footer />
     </div>
   );
 }
